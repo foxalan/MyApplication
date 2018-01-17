4ba2056d7f2cd37d568f97d44d60ac260e295270
alan <swd-004@mail.foxconn.com>
2018-01-17 08:46:34 +0800

    finish user setting

--- mybulter/src/main/java/com/example/mybulter/activity/UserActivity.java
+++ mybulter/src/main/java/com/example/mybulter/activity/UserActivity.java
@@ -1,11 +1,14 @@
 package com.example.mybulter.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.ListViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.example.mybulter.R;
+import com.example.mybulter.adapter.UserAdapter;
 import com.example.mybulter.info.UserBean;
 
 import java.util.ArrayList;
@@ -14,7 +17,6 @@ import java.util.List;
 /**
  * Function :
  * Modify Date : 2018/1/16
- *
  * @Author : Alan
  * Issue : TODO
  * Whether Solve :
@@ -25,12 +27,14 @@ public class UserActivity extends BaseHomeActivity {
     private ListViewCompat mListView;
     private List<UserBean> beanList = new ArrayList<>();
     private String[] items;
+    private AppCompatTextView mTextHeader;
+    private AppCompatImageView mImageHeader;
+    private UserAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
-
         init();
     }
 
@@ -41,15 +45,23 @@ public class UserActivity extends BaseHomeActivity {
 
     private void initView() {
         mListView = (ListViewCompat) findViewById(R.id.lv_user_info);
-        View headView = LayoutInflater.from(this).inflate()
+        View headView = LayoutInflater.from(this).inflate(R.layout.layout_header,null);
+        mTextHeader = (AppCompatTextView) headView.findViewById(R.id.tv_user_head_item);
+        mImageHeader = (AppCompatImageView) headView.findViewById(R.id.iv_user_head_content);
+        mListView.addHeaderView(headView);
     }
 
     private void initData() {
+        mTextHeader.setText("alan");
+        mImageHeader.setImageResource(R.drawable.icon_user);
         beanList.clear();
         items = getResources().getStringArray(R.array.user);
         for (int i = 0; i < items.length; i++) {
             UserBean bean = new UserBean(items[i],"");
+            beanList.add(bean);
         }
+        mAdapter = new UserAdapter(this,beanList,R.layout.item_user_info);
+        mListView.setAdapter(mAdapter);
     }
 
 
