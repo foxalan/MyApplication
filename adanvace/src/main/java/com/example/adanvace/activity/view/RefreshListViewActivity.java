67f0d21875a0fc6b38ed73a602bb700587641a76
alan <swd-004@mail.foxconn.com>
2018-01-19 16:31:38 +0800

    update listview refresh

--- adanvace/src/main/java/com/example/adanvace/activity/view/RefreshListViewActivity.java
+++ adanvace/src/main/java/com/example/adanvace/activity/view/RefreshListViewActivity.java
@@ -1,9 +1,7 @@
 package com.example.adanvace.activity.view;
 
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -12,7 +10,9 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.adanvace.R;
 import com.example.adanvace.adapter.ContactAdapter;
@@ -23,14 +23,15 @@ import java.util.List;
 
 /**
  * Function : listView的下拉刷新
- * Author : Alan
+ *
+ * @Author : Alan
  * Modify Date : 14/8/17
  * Issue : 1.LayoutParams 的使用
  * accomplish: NO
  * Whether solve :
  */
 
-public class RefreshListViewActivity extends BaseActivity {
+public class RefreshListViewActivity extends BaseActivity implements AbsListView.OnScrollListener {
 
     private ListView lv_refresh;
     private LayoutInflater layoutInflater;
@@ -47,12 +48,16 @@ public class RefreshListViewActivity extends BaseActivity {
     private int rl_head_length;
 
     private final static int MSG_BEFORE_REFRESH = 0x123;
+    private final static int MSG_CAN_REFRESH = 0x155;
     private final static int MSG_IS_REFRESH = 0x124;
     private final static int MSG_BACK = 0x125;
-
+    private final static int MSG_HAS_REFRESH = 0X126;
     private LinearLayout.LayoutParams layoutParams;
-
     private int currentTop;
+
+    private TextView tv_refresh;
+    private ProgressBar pb_refresh;
+    private TextView tv_refresh_time;
 
 
     private Handler mHandler = new Handler() {
@@ -61,29 +66,54 @@ public class RefreshListViewActivity extends BaseActivity {
             super.handleMessage(msg);
             switch (msg.what) {
                 case MSG_BEFORE_REFRESH:
-                    lv_refresh.setPadding(rl_header.getPaddingLeft(), scrollLength, rl_header.getPaddingRight(), rl_header.getPaddingBottom());
+                    Log.e("refresh","before refresh");
+                    pb_refresh.setVisibility(View.INVISIBLE);
+                    tv_refresh.setText("下拉刷新");
+                    tv_refresh_time.setVisibility(View.VISIBLE);
+                    rl_header.setPadding(0, scrollLength-rl_head_length, 0, 0);
+                    break;
+                case MSG_CAN_REFRESH:
+                    Log.e("refresh","can refresh");
+                    pb_refresh.setVisibility(View.INVISIBLE);
+                    tv_refresh.setText("放开可以刷新");
+                    tv_refresh_time.setVisibility(View.VISIBLE);
                     break;
                 case MSG_IS_REFRESH:
-                    lv_refresh.layout(lv_refresh.getLeft(), lv_refresh.getTop() + scrollLength - rl_head_length, lv_refresh.getRight(), lv_refresh.getBottom() + scrollLength - rl_head_length);
-
-                    Log.d("TANG", "left:" + lv_refresh.getLeft() + "==top:" + lv_refresh.getTop() + "==right:" + lv_refresh.getRight() + "==bottom:" + lv_refresh.getBottom());
-
+                    Log.e("refresh","is refresh");
+                    pb_refresh.setVisibility(View.VISIBLE);
+                    tv_refresh.setText("正在刷新");
+                    tv_refresh_time.setVisibility(View.GONE);
+                    lv_refresh.layout(lv_refresh.getLeft(), lv_refresh.getTop() + scrollLength, lv_refresh.getRight(), lv_refresh.getBottom() + scrollLength - rl_head_length);
+                    break;
+                case  MSG_HAS_REFRESH:
+                    pb_refresh.setVisibility(View.GONE);
+                    tv_refresh.setText("刷新完成");
+                    tv_refresh_time.setVisibility(View.VISIBLE);
+                    tv_refresh_time.setText(System.currentTimeMillis()+"");
+                    mHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            while (scrollLength != 0) {
+                                mHandler.sendEmptyMessage(MSG_BACK);
+                                try {
+                                    Thread.sleep(5);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                scrollLength--;
+                            }
+                        }
+                    },1000);
                     break;
                 case MSG_BACK:
-                    lv_refresh.layout(lv_refresh.getLeft(), currentTop, lv_refresh.getRight(), lv_refresh.getBottom());
-                    Log.d("TANG", "left:" + lv_refresh.getLeft() + "==top:" + lv_refresh.getTop() + "==right:" + lv_refresh.getRight() + "==bottom:" + lv_refresh.getBottom());
-
+                    Log.e("refresh","back");
+                    rl_header.setPadding(0, scrollLength - rl_head_length, 0, 0);
+                    break;
+                default:
                     break;
             }
         }
     };
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
-    }
 
     @Override
     public int getContentView() {
@@ -92,12 +122,12 @@ public class RefreshListViewActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
         layoutInflater = LayoutInflater.from(this);
         rl_header = (RelativeLayout) layoutInflater.inflate(R.layout.layout_listview_header, null);
-
+        tv_refresh = (TextView) rl_header.findViewById(R.id.tv_refresh);
+        pb_refresh = (ProgressBar) rl_header.findViewById(R.id.pb_refresh);
+        tv_refresh_time = (TextView) rl_header.findViewById(R.id.tv_refresh_time);
         lv_refresh = (ListView) findViewById(R.id.lv_refresh);
-
     }
 
     @Override
@@ -114,85 +144,93 @@ public class RefreshListViewActivity extends BaseActivity {
         }
 
         adapter = new ContactAdapter(this, currentList);
-
+        rl_header.measure(0,0);
         rl_head_length = rl_header.getMeasuredHeight();
-        Log.d("TANG", rl_head_length + "========");
-
+        Log.e("refresh",rl_head_length+"===rl_header_length");
         layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
     }
 
+
+    private int state;
+    private final static int RELEASE_To_REFRESH = 0;
+    private final static int PULL_To_REFRESH = 1;
+    private final static int REFRESHING = 2;
+    private final static int DONE = 3;
+    private final static int LOADING = 4;
+
     @Override
     public void initEvent() {
+
         lv_refresh.setAdapter(adapter);
         lv_refresh.addHeaderView(rl_header);
-
-        Log.d("TANG", "left:" + lv_refresh.getLeft() + "==top:" + lv_refresh.getTop() + "==right:" + lv_refresh.getRight() + "==bottom:" + lv_refresh.getBottom());
-
         lv_refresh.setSelection(1);
-
-        Log.d("TANG", "left:" + lv_refresh.getLeft() + "==top:" + lv_refresh.getTop() + "==right:" + lv_refresh.getRight() + "==bottom:" + lv_refresh.getBottom());
-
-
-        lv_refresh.setOnScrollListener(new AbsListView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-
-
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-
-            }
-        });
 
         lv_refresh.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
-
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-
                         startY = (int) event.getY();
-
                         break;
                     case MotionEvent.ACTION_MOVE:
-
+                        if (isRefreshable){}
+                        int scroll = lv_refresh.getScrollY();
                         int moveY = (int) event.getY();
-                        scrollLength = moveY - startY;
-                        Log.d("TANG", scrollLength + "================" + rl_head_length);
+                        scrollLength = moveY - startY + scroll;
+                        Log.e("refresh", "tang" + "===" + scrollLength+"==="+rl_head_length);
+                        if (scrollLength < (rl_head_length + 10)) {
+                            state = RELEASE_To_REFRESH;
+                            mHandler.sendEmptyMessage(MSG_BEFORE_REFRESH);
 
-                        mHandler.sendEmptyMessage(MSG_IS_REFRESH);
-
+                        } else {
+                            state = PULL_To_REFRESH;
+                            mHandler.sendEmptyMessage(MSG_CAN_REFRESH);
+                        }
 
                         break;
 
                     case MotionEvent.ACTION_UP:
                         currentTop = lv_refresh.getTop();
-                        Log.d("TANG", "ACTION_UP");
-                        new Thread() {
-                            @Override
-                            public void run() {
-                                super.run();
-                                while (currentTop != 0) {
-                                    Log.d("TANG", lv_refresh.getTop() + "======================");
-                                    mHandler.sendEmptyMessage(MSG_BACK);
-                                    try {
-                                        sleep(5);
-                                    } catch (InterruptedException e) {
-                                        e.printStackTrace();
+                        switch (state) {
+                            case RELEASE_To_REFRESH:
+                                state = DONE;
+                                new Thread() {
+                                    @Override
+                                    public void run() {
+                                        super.run();
+                                        while (scrollLength != 0) {
+                                            mHandler.sendEmptyMessage(MSG_BACK);
+                                            try {
+                                                sleep(5);
+                                            } catch (InterruptedException e) {
+                                                e.printStackTrace();
+                                            }
+                                            scrollLength--;
+                                        }
+
                                     }
+                                }.start();
+                                break;
+                            case PULL_To_REFRESH:
+                                state = REFRESHING;
+                                mHandler.sendEmptyMessage(MSG_IS_REFRESH);
+                                mHandler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        currentList.add(new ContactInfo(false,false,"huiye","die"));
+                                        state = DONE;
+                                        adapter.notifyDataSetChanged();
+                                        mHandler.sendEmptyMessage(MSG_HAS_REFRESH);
 
-                                    currentTop--;
-                                }
 
-                            }
-                        }.start();
-
+                                    }
+                                },2000);
+                                break;
+                        }
+                        break;
+                    default:
                         break;
                 }
-
 
                 return false;
             }
@@ -200,4 +238,19 @@ public class RefreshListViewActivity extends BaseActivity {
     }
 
 
+    boolean isRefreshable;
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        if (firstVisibleItem == 0) {
+            isRefreshable = true;
+        } else {
+            isRefreshable = false;
+        }
+    }
 }
